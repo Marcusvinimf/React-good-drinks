04b2c8af4a41a7b74d4b32712957ba9e9c8f6a2a
Marcusvinimf <marcus_v_ina@hotmail.com>
2021-03-04 18:06:38 -0300

    adicionando funcionalidades de botão e de setInterval

--- src/componentesContato/cabeca/Fim.js
+++ src/componentesContato/cabeca/Fim.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Fim extends Component {
 
-    constructor(){
-          super()
-          this.state = {date: new Date()}
-          console.log("Fui iniciado")
-      }
+    constructor(props){
+        super(props);
+        this.timeOnOff = this.timeOnOff.bind(this);
+        this.state = {date: new Date()};
+        console.log("Fui iniciado");
+    }
+    
+    timeOnOff() {
+        this.setState({date: new Date()});
+    }
+    
+    componentDidMount(){
+        this.timerID =  setInterval(this.timeOnOff, 1000);
+        console.log("Fui montado");
+    }
+    
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+        console.log("vou ser desmontado");
+    }
 
-      componentDidMount(){
-          console.log("Fui montado")
-      }
-
-      componentWillUnmount(){
-        console.log("vou ser desmontando")
-      }
-
-      render() {
-          console.log("Fui renderizado")
-          return (
-              <div>
-                    <></>
-              </div>
-          )
-      }
+    render() {
+        console.log("Fui renderizado");
+        return (
+            <h1>
+                Vms ver no que vai dar {this.state.date.toLocaleTimeString()}
+            </h1>
+        )
+    }
 }
--- src/componentesContato/corpo/Formulario.js
+++ src/componentesContato/corpo/Formulario.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react'
 
 export default class Formulario extends Component {
+    constructor(props) {
+        super(props)
+        this.click = this.click.bind(this)
+        this.state = { estadoOnOff: false }
+        console.log("Fui iniciado")
+    }
+
+    click() {
+        this.setState((state) => { return { estadoOnOff: true } });
+        setTimeout(() => this.setState({ estadoOnOff: false }), 3000)
+    }
+
+    componentDidMount() {
+        console.log("Fui montado");
+    }
+
     render() {
+        console.log("Fui renderizado");
         return (
             <div>
                 <h2>Contato</h2>
@@ -9,12 +26,12 @@ export default class Formulario extends Component {
                     <img className="imagem1" src="https://bonsdrinks.netlify.app/static/media/contact-pic.689e126c.jpg"></img>
                     <form className="forms">
                         <label >nome: </label>
-                        <input/>
+                        <input />
                         <label>e-mail: </label>
-                        <input/>
+                        <input />
                         <label>mensagem: </label>
-                        <textarea name="mensagem" rows="10" cols="33"/>
-                        <button className="botao">Enviar</button>
+                        <textarea name="mensagem" rows="10" cols="33" />
+                        <button className="botao" disabled={this.state.estadoOnOff} onClick={this.click}>Enviar</button>
                     </form>
                 </div>
             </div>
